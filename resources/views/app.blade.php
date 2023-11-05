572c878b9c0f537de68559305fd4eb98bf168c4d
Johan <johanclement88@hotmail.fr>
2023-11-05 15:14:17 +0100

    ajout d'un favicon au projet

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
     <title>TS Notes</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
